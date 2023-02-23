ecc1db441e5a50615a14d1726c26cc1355a9bd07
partezan7 <partezan7@rambler.ru>
2023-02-24 01:14:36 +0300

    Add new page

--- src/main/java/ru/partezan7/proto/prototype/controller/ProtoController.java
+++ src/main/java/ru/partezan7/proto/prototype/controller/ProtoController.java
@@ -5,12 +5,20 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.Map;
+
 @Controller
 public class ProtoController {
     @GetMapping("/start")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="User") String name, Model model) {
         model.addAttribute("name", name);
         return "start-page";
+    }
+
+    @GetMapping("/")
+    public String mainPage(Map<String, Object> model){
+        model.put("info", "This is the main page");
+        return "main-page";
     }
 
 }
